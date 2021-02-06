91629b95760b7196c5509bade02eb33cc00be4dc
Ilya Timoshenko <timoh.ilya@vis-design.com>
2021-02-06 21:29:02 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+import '../sass/main'
+
+console.log("12")
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+//
+const optimization = () => {
+    const config = {
+        //Оптимизация общих библеотек из разных файлов 
+        // splitChunks: {
+        //     chunks: 'all'
+        // },
+    }
+    if(isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: ['@babel/polyfill', './js/main.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: optimization(),
+    resolve: {
+        extensions: ['.js', '.sass']
+    },
+    devServer: {
+        port: 4200
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets'),
+                    to: path.resolve(__dirname, 'dist/assets')
+                }
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.sass$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+        ]
+    }
+}
